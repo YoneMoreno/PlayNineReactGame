826dd36c2edd66a863533caa78eddb259b622e6e
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-12 18:13:20 +0000

    unselectNumber function to remove clicked Answers back to Numbers

--- src/Answer.js
+++ src/Answer.js
@@ -5,7 +5,7 @@ const Answer = (props) => {
     return (
         <div className="col-5">
             {props.selectedNumbers.map((number, i) =>
-                <span key={i}>{number}</span>
+                <span key={i} onClick={() => props.unselectNumber(number)}>{number}</span>
             )}
         </div>
     );
