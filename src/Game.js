59c9ef6ce7ebb81b5c89bc3fbd7d71455cdfd837
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-12 15:06:33 +0000

    Game component

---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Game extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>Play Nine</h3>
+            </div>
+        );
+    }
+}
+export {Game}
