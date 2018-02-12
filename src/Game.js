95444fe119ac92fc855293e20b0e669e10b00abf
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-12 15:09:17 +0000

    stars component

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Stars} from "./Stars";
 
 class Game extends React.Component{
     render(){
         return(
             <div>
                 <h3>Play Nine</h3>
+                <Stars/>
             </div>
         );
     }
