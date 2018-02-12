719524c7f38bea1bb8acd26750b2d5e22a094cc9
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-12 15:10:59 +0000

    button component

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Stars} from "./Stars";
+import {Button} from "./Button";
 
-class Game extends React.Component{
-    render(){
-        return(
+class Game extends React.Component {
+    render() {
+        return (
             <div>
                 <h3>Play Nine</h3>
                 <Stars/>
+                <Button/>
             </div>
         );
     }
 }
+
 export {Game}
