f5b0879df110bce962d0e16686682a8114b130ae
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-12 15:15:33 +0000

    add font awesome

--- src/Stars.js
+++ src/Stars.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Stars = () => {
     return (
         <div>
-            ...
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
         </div>
     );
 };
