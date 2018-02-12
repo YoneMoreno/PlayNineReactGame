db48823b84fe8b6652f9155f55c0aecb9775235c
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-12 15:51:17 +0000

    display dinamic number of starts

--- src/Stars.js
+++ src/Stars.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Stars = () => {
+    const numberOfStars = 5;
+
+    let stars = [];
+    for (let i = 0; i < numberOfStars; i++) {
+        stars.push(<i className="fa fa-star"></i>);
+    }
+
     return (
         <div className="col-5">
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
+            {stars}
         </div>
     );
 };
