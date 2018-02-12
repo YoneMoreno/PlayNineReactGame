d8998e5538d11ee774798b642223c0b1a7b6d2a5
Yone <yone.moreno101@alu.ulpgc.es>
2018-02-12 15:45:42 +0000

    numbers classes

--- src/Numbers.js
+++ src/Numbers.js
@@ -5,8 +5,8 @@ const Numbers = () => {
         <div className="card text-center">
             <div>
                 <span>1</span>
-                <span>2</span>
-                <span>3</span>
+                <span className="selected">2</span>
+                <span className="used">3</span>
             </div>
         </div>
     );
